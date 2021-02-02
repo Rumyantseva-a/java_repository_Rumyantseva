f374f29b51d4c42ce29a331cdbf488989e74b188
Rumyantseva-a <absoade@mail.ru>
2021-02-02 20:20:57 +0300

    Внесены исправления в задание

--- sandbox/src/main/java/rumyantseva/sandbox/MySecondProgram.java
+++ sandbox/src/main/java/rumyantseva/sandbox/MySecondProgram.java
@@ -25,6 +25,42 @@ public class MySecondProgram {
     System.out.println ("Расстояние между точками p1 и p2 = " + distancefunc(dd));
 
 
+    Point p1 = new Point();
+    Point p2 = new Point();
+    p1.x = 4;
+    p2.x = 8;
+    p1.y = 5;
+    p2.y = 12;
+    System.out.println ("3.Расстояние между точками p1 и p2 = " + distance(p1, p2));
+
+    System.out.println ("4.Расстояние между точками p1 и p2 = " + p1.distanceconstrmet1(p2));
+
+
+
+    Point p3 = new Point();
+    Point p4 = new Point();
+    p3.x = 5;
+    p4.x = 10;
+    p3.y = 15;
+    p4.y = 20;
+    System.out.println ("4. Расстояние между точками p3 и p4 = " + p3.distanceconstrmet1(p4));
+
+
+
+
+    Point p5 = new Point();
+    p5.x = 30;
+    p5.y = 50;
+    x1 = 200;
+    System.out.println ("4. Расстояние между точками p5 и произвольной точкой = " + p5.distanceconstrmet2(x1, y1));
+
+
+
+
+
+
+
+
     Pointdistanceconstr pp  = new Pointdistanceconstr(4,8,5,12);
     System.out.println ("Расстояние между точками p1 и p2 = " + distancefunc(pp));
 
@@ -43,6 +79,13 @@ public class MySecondProgram {
 
   public static double distancefunc (Pointdistanceconstr pp) {
     return Math.sqrt((pp.x2 - pp.x1) * (pp.y2 - pp.y1));
+
+  }
+
+  //2
+  public static double distance (Point p1, Point p2) {
+    return Math.sqrt((p2.x - p1.x) * (p2.y - p1.y));
+
 
   }
 
--- sandbox/src/main/java/rumyantseva/sandbox/Point.java
+++ sandbox/src/main/java/rumyantseva/sandbox/Point.java
@@ -5,4 +5,15 @@ public class Point {
   public double x;
   public double y;
 
+
+  public double distanceconstrmet1(Point p) {
+    return Math.sqrt((p.x - this.x) * (p.y - this.y));
+
+  }
+
+  public double distanceconstrmet2(double a, double b) {
+//    return Math.sqrt((a - this.x) * (b - this.y));
+    return Math.sqrt( Math.abs(a - this.x) * Math.abs(b - this.y));
+
+  }
 }
