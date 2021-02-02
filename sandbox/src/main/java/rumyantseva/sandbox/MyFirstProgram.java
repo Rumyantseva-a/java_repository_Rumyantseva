d18b3538930a227b01bf9095146a49211ed809b8
Rumyantseva-a <absoade@mail.ru>
2021-02-02 12:38:15 +0300

    --

--- sandbox/src/main/java/rumyantseva/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/rumyantseva/sandbox/MyFirstProgram.java
@@ -3,7 +3,13 @@ package rumyantseva.sandbox;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello" + "world!");
+
+        System.out.println("Площадь квадрата со стороной " + 5 + " = " + (5 * 5));
+
+        int l = 8;
+        int s = l * l;
+        System.out.println("Площадь квадрата со стороной " + l + " = " + s);
     }
 
 }
