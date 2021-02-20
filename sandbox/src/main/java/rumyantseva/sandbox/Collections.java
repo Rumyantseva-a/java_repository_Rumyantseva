527b1ab51819f90db21cf8f6d289875fa6187082
Rumyantseva-a <absoade@mail.ru>
2021-02-20 09:28:33 +0300

    Изменен класс Collections

--- sandbox/src/main/java/rumyantseva/sandbox/Collections.java
+++ sandbox/src/main/java/rumyantseva/sandbox/Collections.java
@@ -1,12 +1,28 @@
 package rumyantseva.sandbox;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Collections {
 
-  public static void main (String [] args) {
-    String [] langs = new String [4];
-    langs[0] = "java";
-    langs[1] = "C#";
-    langs[2] = "Python";
-    langs[3] = "PHP";
+  //public static void main(String[] args) {
+  //  String[] langs = {"java", "C#", "Python", "PHP"};
+  //  for (int i = 0; i < langs.length; i++) {
+  //    System.out.println("Я хочу выучить " + langs[i]);
+  //  }
+  //}
+
+  public static void main(String[] args) {
+    String[] langs = {"java", "C#", "Python", "PHP"};
+    List<String> languages = Arrays.asList("Java", "C#", "Python");
+
+    //for (String l : languages) {
+    //  System.out.println("Я хочу выучить " + l);
+    //}
+
+    for (String l : languages){
+      System.out.println("Я хочу выучить " + l);
+    }
   }
 }
