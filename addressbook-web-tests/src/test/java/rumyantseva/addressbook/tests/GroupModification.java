9dcbfa3a408d7b4f54d20e36e8f03d9334949f41
Rumyantseva-a <absoade@mail.ru>
2021-03-10 10:27:30 +0300

    Добавлено упорядочивание списков в тестах для создания и модификации групп

--- addressbook-web-tests/src/test/java/rumyantseva/addressbook/tests/GroupModification.java
+++ addressbook-web-tests/src/test/java/rumyantseva/addressbook/tests/GroupModification.java
@@ -1,8 +1,11 @@
 package rumyantseva.addressbook.tests;
 
+import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import rumyantseva.addressbook.model.GroupData;
+
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -27,7 +30,10 @@ public class GroupModification extends TestBase {
 
     before.remove (before.size() - 1);
     before.add(group);
-    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+    Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
+    before.sort(byId);
+    after.sort(byId);
+    Assert.assertEquals(before, after);
   }
 }
 
