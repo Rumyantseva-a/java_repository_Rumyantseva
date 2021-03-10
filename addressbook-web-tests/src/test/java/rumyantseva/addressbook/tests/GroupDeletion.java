8dcae83637ac54aa777d224872d16c1e480ca2db
Rumyantseva-a <absoade@mail.ru>
2021-03-10 09:16:14 +0300

    Добавлено сравнение списков в тестах групп

--- addressbook-web-tests/src/test/java/rumyantseva/addressbook/tests/GroupDeletion.java
+++ addressbook-web-tests/src/test/java/rumyantseva/addressbook/tests/GroupDeletion.java
@@ -19,7 +19,13 @@ public class GroupDeletion extends TestBase{
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();
     List<GroupData> after = app.getGroupHelper().getGroupList();
-    Assert.assertEquals(after.size(), before.size() + 1);
+    Assert.assertEquals(after.size(), before.size() - 1);
+
+    before.remove (before.size() - 1);
+    for (int i = 0; i < after.size(); i++) {
+      Assert.assertEquals(before, after);
+
+    }
   }
 
 
--- addressbook-web-tests/src/test/java/rumyantseva/addressbook/tests/GroupModification.java
+++ addressbook-web-tests/src/test/java/rumyantseva/addressbook/tests/GroupModification.java
@@ -3,7 +3,7 @@ package rumyantseva.addressbook.tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import rumyantseva.addressbook.model.GroupData;
-
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModification extends TestBase {
@@ -18,11 +18,16 @@ public class GroupModification extends TestBase {
     List<GroupData> before = app.getGroupHelper().getGroupList();
     app.getGroupHelper().selectGroup(before.size() - 1);
     app.getGroupHelper().initGroupModification();
-    app.getGroupHelper().fillGroupForm(new GroupData("5ModPyataya1", "rty", "rty"));
+    GroupData group = new GroupData(before.get(before.size() - 1).getId(), "1odPuataya1", "rty", "rty");
+    app.getGroupHelper().fillGroupForm(group);
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size());
+
+    before.remove (before.size() - 1);
+    before.add(group);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
 }
 
