677946f0fd0421e43ba2ed1e3d80d13e4fdd6fd2
Rumyantseva-a <absoade@mail.ru>
2021-03-10 09:41:12 +0300

    Оптимизирован спосб вычисления максимального идентификатора групп

--- addressbook-web-tests/src/test/java/rumyantseva/addressbook/tests/Groupcreation.java
+++ addressbook-web-tests/src/test/java/rumyantseva/addressbook/tests/Groupcreation.java
@@ -3,7 +3,6 @@ package rumyantseva.addressbook.tests;
 import org.testng.Assert;
 import org.testng.annotations.*;
 import rumyantseva.addressbook.model.GroupData;
-
 import java.util.HashSet;
 import java.util.List;
 
@@ -13,19 +12,21 @@ public class Groupcreation extends TestBase{
   public void testGroupcreation() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
     List<GroupData> before = app.getGroupHelper().getGroupList();
-    GroupData group = new GroupData("New", "Meow", "Yau");
+    GroupData group = new GroupData("Newlambda", "Meow", "Yau");
     app.getGroupHelper().createGroup(group);
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() + 1);
 
 
-    int max = 0;
-    for (GroupData g : after) {
-      if (g.getId() > max) {
-        max = g.getId();
-      }
-    }
-    group.setId(max);
+    //старый способ - с помощью цикла
+    //int max = 0;
+    //for (GroupData g : after) {
+    //  if (g.getId() > max) {
+    //    max = g.getId();
+    //  }
+    //}
+
+    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(group);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
