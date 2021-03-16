17853546fc923ec0962d68c31c9438237cc60cf6
Rumyantseva-a <absoade@mail.ru>
2021-03-16 22:48:08 +0300

    Сортировка списка по индексу заменена на ид в группах

--- addressbook-web-tests/src/test/java/rumyantseva/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/rumyantseva/addressbook/appmanager/GroupHelper.java
@@ -3,11 +3,12 @@ package rumyantseva.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import rumyantseva.addressbook.model.GroupData;
-
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
 
 public class GroupHelper extends HelperBase{
 
@@ -37,14 +38,12 @@ public class GroupHelper extends HelperBase{
     click(By.name("delete"));
   }
 
-  public void selectGroup(int index) {
-    wd.findElements(By.name("selected[]")).get(index).click();
-    //click(By.name("selected[]"));
+  public void selectGroupById(int id) {
+    wd.findElement(By.cssSelector("input[value='" + id + "']")).click();
   }
 
   public void initGroupModification() {
     click(By.name("edit"));
-    //click(By.xpath(//input[@name='edit'])[2]);
   }
 
   public void submitGroupModification() {
@@ -58,19 +57,22 @@ public class GroupHelper extends HelperBase{
     returnToGroupPage();
   }
 
-  public void modify(int index, GroupData group) {
-    selectGroup(index);
+  public void modify(GroupData group) {
+    selectGroupById(group.getId());
     initGroupModification();
     fillGroupForm(group);
     submitGroupModification();
     returnToGroupPage();
   }
 
-  public void delete(int index) {
-    selectGroup(index);
+
+  public void delete(GroupData group) {
+    selectGroupById(group.getId());
     deleteSelectedGroups();
     returnToGroupPage();
   }
+
+
 
   public boolean isThereaGroup() {
     return isElementPresent(By.name("selected[]"));
@@ -80,8 +82,18 @@ public class GroupHelper extends HelperBase{
     return wd.findElements(By.name("selected[]")).size();
   }
 
-  public List<GroupData> list() {
-    List<GroupData> groups = new ArrayList<GroupData>();
+  //public List<GroupData> list() {
+  //  List<GroupData> groups = new ArrayList<GroupData>();
+  //  List<WebElement> elements = wd.findElements (By.cssSelector("span.group"));
+  //  for (WebElement element : elements) {
+  //    String name = element.getText();
+  //    int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("value"));
+  //    groups.add(new GroupData().withId(id).withName(name));
+  //  }
+  //  return groups; }
+
+  public Set<GroupData> all() {
+    Set<GroupData> groups = new HashSet<GroupData>();
     List<WebElement> elements = wd.findElements (By.cssSelector("span.group"));
     for (WebElement element : elements) {
       String name = element.getText();
@@ -91,4 +103,5 @@ public class GroupHelper extends HelperBase{
 
     return groups;
   }
+
 }
