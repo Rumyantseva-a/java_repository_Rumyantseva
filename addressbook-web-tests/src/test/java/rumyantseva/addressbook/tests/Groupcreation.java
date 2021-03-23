5279814ed62453ec7ba22cca88a89a313b3519f3
Rumyantseva-a <absoade@mail.ru>
2021-03-23 20:32:59 +0300

    Добавлен и использован ДатаПровайдер для групп

--- addressbook-web-tests/src/test/java/rumyantseva/addressbook/tests/Groupcreation.java
+++ addressbook-web-tests/src/test/java/rumyantseva/addressbook/tests/Groupcreation.java
@@ -3,22 +3,36 @@ package rumyantseva.addressbook.tests;
 import org.testng.annotations.*;
 import rumyantseva.addressbook.model.GroupData;
 import rumyantseva.addressbook.model.Groups;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.*;
 
 public class Groupcreation extends TestBase{
 
-  @Test
-  public void testGroupCreation() throws Exception {
+  @DataProvider
+  public Iterator<Object[]> validGroups() {
+    List<Object[]> list = new ArrayList<Object[]>();
+    list.add(new Object[] {new GroupData().withName("test1").withHeader("header 1").withFooter("footer 1")} );
+    list.add(new Object[] {new GroupData().withName("test2").withHeader("header 2").withFooter("footer 2")} );
+    list.add(new Object[] {new GroupData().withName("test3").withHeader("header 3").withFooter("footer 3")} );
+    return list.iterator();
+  }
+
+  @Test (dataProvider = "validGroups")
+  public void testGroupCreation(GroupData group) throws Exception {
     app.goTo().groupPage();
     Groups before = app.group().all();
-    GroupData group = new GroupData().withName("test2");
     app.group().create(group);
     assertThat(app.group().count(), equalTo(before.size() + 1));
     Groups after = app.group().all();
     assertThat(after, equalTo(
             before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
-  }
+    }
+
 
   @Test
   public void testBadGroupCreation() throws Exception {
