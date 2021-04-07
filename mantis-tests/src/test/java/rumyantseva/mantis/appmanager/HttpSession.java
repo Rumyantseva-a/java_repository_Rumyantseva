feec3c003239b2a2b8e0bdbb29fb376669fda4f2
Rumyantseva-a <absoade@mail.ru>
2021-04-07 23:01:36 +0300

    HttpSession

---
+++ mantis-tests/src/test/java/rumyantseva/mantis/appmanager/HttpSession.java
@@ -0,0 +1,43 @@
+package rumyantseva.mantis.appmanager;
+
+import java.io.IOException;
+
+public class HttpSession {
+  private CloseableHttpClient httpclient;
+  private ApplicationManager app;
+
+  public HttpSession(ApplicationManager app) {
+    this.app = app;
+    httpclient = HttpClients.custom().setRedirectStrategy(new LaxRedirectStrategy()).build();
+  }
+
+  public boolean login (String username, String password) throws IOException {
+    HttpPost post = new HttpPost(app.getProperty("web.baseUrl") + "/login.php");
+    List<NameValuePair> params = new ArrayList<NameValuePair>();
+    params.add(new BasicNameValuePair ("username", username));
+    params.add(new BasicNameValuePair ("password", password));
+    params.add(new BasicNameValuePair ("secure_session", "on"));
+    params.add(new BasicNameValuePair ("return", "index.php"));
+    post.setEntity(new UrlEncodedFormEntity(params));
+    CloseableHttpsResponse response = httpclient.execute(post);
+    String body = geTextForm(response);
+    return body.contains(String.format("<span class=\"italic\">%s</span>", username));
+  }
+
+  private String getTextFrom(CloseableHttpResponse response) throws IOExeption {
+    try {
+      return EntityUtils.toString(response.getEntity());
+    } finally {
+      response.close();
+    }
+  }
+
+  public boolean isLoggedInAs(String username) throws IOExeption {
+    HttpGet get = new HttpGet(app.getProperty("web.baseUrl") + "/index.php");
+    CloseableHttpResponse response = httpclient.execute(get);
+    String body = getTextFrom(response);
+    return body.contains(String.format("<span class=\"italic\">%s</span>", username));
+  }
+
+
+}
